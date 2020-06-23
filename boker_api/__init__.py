b8c7aa0e098694a8c05cdf0ce346d3f95b943425
Hayk Khachatryan <hi@hayk.io>
2020-06-23 17:43:51 +0400

    Hello routers.topup & routers.balances

--- boker_api/__init__.py
+++ boker_api/__init__.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 
+from boker_api.routers import topup, balances
+
 app = FastAPI()
+
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+app.include_router(
+    topup.router,
+    prefix="/topup",
+    tags=["topup"]
+)
+app.include_router(
+    balances.router,
+    prefix="/balances",
+    tags=["balances"]
+)
