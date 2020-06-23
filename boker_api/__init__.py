25604adde464688874ff3d1fe05e44d7ffec3388
Hayk Khachatryan <hi@hayk.io>
2020-06-23 20:16:20 +0400

    Hello discord.py
    boker_api.bot:
      - inits discord.py sample client
    
    boker_api:
      - loads .env variables
      - imports boker_api.bot
        - asyncio.create_tasks bot client start

--- boker_api/__init__.py
+++ boker_api/__init__.py
@@ -1,8 +1,19 @@
+import os
+import asyncio
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from boker_api.routers import topup, balances
+from boker_api.bot import client
+
+load_dotenv()
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_event():
+    asyncio.create_task(client.start(os.getenv('DISCORD_BOT_TOKEN')))
 
 
 @app.get("/")
