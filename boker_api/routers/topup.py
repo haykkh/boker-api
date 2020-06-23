31b62c46a7212b50a32a5a1a51d88304b37a6b2e
Hayk Khachatryan <hi@hayk.io>
2020-06-23 21:49:21 +0400

    topup.py
    class TopUp:
      - pydantic class
      - attributes:
        - identification: discord user-id
        - amount: number of chips to top up
    
    post_topup():
      - takes a TopUp object as request body
      - sends discord message to channel after parsing details

--- boker_api/routers/topup.py
+++ boker_api/routers/topup.py
@@ -1,8 +1,19 @@
+import os
 from fastapi import APIRouter
+from pydantic import BaseModel
+
+from boker_api.bot import client
 
 router = APIRouter()
 
 
-@router.get("/")
-async def read_topup():
-    return {"Hello": "topup"}
+class TopUp(BaseModel):
+    identification: str
+    amount: int
+
+
+@router.post("/")
+async def post_topup(topup: TopUp):
+    channel = client.get_channel(int(os.getenv('BOT_CHANNEL')))
+    await channel.send(f'pac <@{topup.identification}> {topup.amount}')
+    return f'!pac <@{topup.identification}> {topup.amount}'
